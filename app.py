42cc0193ec6dfa76aec57ae87ae1fb6fd8249b5d
sherly <sharton1@asu.edu>
2022-07-26 18:41:10 -0400

    add app files

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, Response
+from camera import Video 
+
+app=Flask(__name__)
+
+# create home route
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+def generate_frame(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type:  image/jpeg\r\n\r\n' + frame +
+              b'\r\n\r\n')
+
+# get image from video
+@app.route("/video")
+def video():
+    
+    # return frame to front end
+    return Response(generate_frame(Video()),
+    mimetype = 'multipart/x-mixed-replace; boundary=frame')
+
+app.run(debug=True)
--- track.py
+++ track.py
@@ -4,34 +4,41 @@ from object_detection import ObjectDetection
 from deep_sort.deep_sort import Deep
 import torch
 
+# !pip install --upgrade pip
+# !pip install -r requirements.txt
+# !pip install -r https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt
+# !pip3 install --upgrade tensorflow==2.4
+
+
 def detect(model, frame):
-  '''Given results from yolov5, extract classs ids, scores and boxes as numpy array'''
+    '''Given results from yolov5, extract classs ids, scores and boxes as numpy array'''
 
-  results = model(frame)
-  result_pandas = results.pandas().xyxy[0]
-  print(result_pandas)
-  result_list = results.xyxy[0].cpu().detach().tolist()
-  class_ids = []
-  scores = []
-  boxes = []
+    results = model(frame)
+    result_pandas = results.pandas().xyxy[0]
+    print(result_pandas)
+    result_list = results.xyxy[0].cpu().detach().tolist()
+    class_ids = []
+    scores = []
+    boxes = []
 
-  for i in range(len(result_list)):
-    class_ids.append(result_list[i][5])
-    scores.append(result_list[i][4])
-    boxes.append(result_list[i][0:4])
+    for i in range(len(result_list)):
+        class_ids.append(result_list[i][5])
+        scores.append(result_list[i][4])
+        boxes.append(result_list[i][0:4])
 
-  class_ids = np.asarray(class_ids, dtype=np.int32)
-  scores = np.asarray(scores, dtype=np.float64)
-  boxes = np.asarray(boxes, dtype=np.float64)
+    class_ids = np.asarray(class_ids, dtype=np.int32)
+    scores = np.asarray(scores, dtype=np.float64)
+    boxes = np.asarray(boxes, dtype=np.float64)
 
-  return class_ids, scores, boxes
+    return class_ids, scores, boxes
+
 
 # Load Object Detection
 yolov4 = ObjectDetection("dnn_model/yolov4.weights", "dnn_model/yolov4.cfg")
 yolov4.load_class_names("dnn_model/classes.txt")
-yolov4.load_detection_model(image_size=832, # 416 - 1280
-                        nmsThreshold=0.4,
-                        confThreshold=0.3)
+yolov4.load_detection_model(image_size=832,  # 416 - 1280
+                            nmsThreshold=0.4,
+                            confThreshold=0.3)
 # yolov5 = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 
@@ -44,7 +51,7 @@ deep = Deep(max_distance=0.7,
 tracker = deep.sort_tracker()
 
 
-# capture frame 
+# capture frame
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -57,12 +64,12 @@ while True:
     # >>>>>>>>>>>>>>>>>>>>>>>>>> yoloV5 >>>>>>>>>>>>>>>>>>
     # (class_ids, scores, boxes) = detect(yolov5, frame)
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    
+
     (class_ids, scores, boxes) = yolov4.detect(frame)
     print("class_ids>>>>>>>>>>>>>>", class_ids)
     print("scores>>>>>>>>>>>>>>", scores)
     print("boxes>>>>>>>>>>>>>>", boxes)
-    
+
     """ 2. Object Tracking """
     features = deep.encoder(frame, boxes)
     detections = deep.Detection(boxes, scores, class_ids, features)
@@ -80,8 +87,10 @@ while True:
         color = yolov4.colors[class_id]
 
         cv2.rectangle(frame, (x, y), (x2, y2), color, 2)
-        cv2.rectangle(frame, (x, y), (x + len(class_name) * 20, y - 30), color, -1)
-        cv2.putText(frame, class_name + " " + str(object_id), (x, y - 10), 0, 0.75, (255, 255, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + len(class_name)
+                      * 20, y - 30), color, -1)
+        cv2.putText(frame, class_name + " " + str(object_id),
+                    (x, y - 10), 0, 0.75, (255, 255, 255), 2)
 
     cv2.imshow("Frame", frame)
 
